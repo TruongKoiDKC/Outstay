74391264585a5726ad4e9a87692093b74e92f071
TruongKoiDKC <61343185+TruongKoiDKC@users.noreply.github.com>
2020-03-29 10:37:09 +0700

    Chỉnh sửa DichVu_Them.js

--- src/ManHinh/DichVu_Them.js
+++ src/ManHinh/DichVu_Them.js
@@ -1,139 +1,140 @@
-import React from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  Image,
-  Dimensions,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  ScrollView,
-  
-} from "react-native";
-import { Dropdown } from 'react-native-material-dropdown';
+import React, { Component } from 'react'
+import { View,
+    Text,
+    TextInput,
+    StyleSheet,
+    Image,
+    Dimensions,
+    TouchableOpacity,
+    StatusBar,
+    ImageBackground, 
+} from 'react-native'
+
+import { ScrollView } from 'react-native-gesture-handler'
+import Animated from 'react-native-reanimated';
+import RNPickerSelect from 'react-native-picker-select';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Foundation from 'react-native-vector-icons/Foundation'
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+
 import { Jiro } from 'react-native-textinput-effects';
-//Thư viện giúp keyboard ko che holder textinput
 
-//import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+//Sửa label text 
+const placeholder = {
+    label: 'Lựa chọn...',
+    color:'gray',
+    value: null,
+};
 
-//thay đôỉ componentWill.. thành UNSAFE_component.....
-//nó chỉ cài package kia mà t cài nó vâ4n bị
-const DEPRECATED_APIS = Object.create(null);
-DEPRECATED_APIS.componentWillReceiveProps = 'UNSAFE_componentWillReceiveProps';
-DEPRECATED_APIS.componentWillUpdate = 'UNSAFE_componentWillUpdate';
-//
-
-export default class Home extends React.Component {
-    render() {
-            let data = [{
-              value: 'Banana',
-            }, {
-              value: 'Mango',
-            }, {
-              value: 'Khỉ',
-            }];
-        return(
-        <View style={styles.container}>
-
-            <View style={styles.header}>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate("ManHinhDichVu")}>
-                    <Image
-                        source={require("../images/iconfinder_left_2_4829870.png")}
-                        style={{width: 25, height: 25,marginRight: 100}}
-                    >
-                    </Image>
-                </TouchableOpacity>    
-
-                    <Text style={{color:'black', fontSize: 25, fontWeight:'bold',marginRight:100}}>Dịch vụ</Text>
-
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate("ManHinhHome")}>
+export default class Chitiet_PhongTro extends Component {
+    render () {
+        return (
+            <View style={styles.container}>
+                <View style={{flexDirection: 'row', justifyContent:"center", alignItems:"center"}}>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate("ManHinhDichVu")}>
                         <Image
-                            source={require("../images/iconfinder_checkmark_4115228.png")}
-                            style={{width: 25, height: 25}}
+                            source={require("../images/iconfinder_left_2_4829870.png")}
+                            style={{width: 25, height: 25,marginRight: 100}}
                         >
                         </Image>
-                </TouchableOpacity>
+                    </TouchableOpacity>
 
-            </View>  
+                    <View style={{flex: 3, alignItems:"center"}}>
+                        <Text style={{color:'black', fontSize: 21, fontWeight:'bold',marginRight: 60}}>
+                            Dịch vụ 
+                        </Text>
+                    </View>
 
-            <View style={styles.body}>
-                <Dropdown
-                    label='Loại hình và dịch vụ '
-                    style={{fontStyle: 'italic'}}
-                    data={data}
-                    style={styles.Dropdown}
-                />
+                    <View style={{flex: 1}}>
+                        <TouchableOpacity onPress={() => this.props.navigation.navigate('ManHinhHome')}>
+                            <ImageBackground source={require('../images/iconfinder_checkmark_4115228.png')}
+                                style={{width: 30, height: 30, marginLeft:"50%"}}>
+                            </ImageBackground>
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
-                <Jiro
-                    label={'Tên dịch vụ'}
-                    borderColor={'#5aaf76'}
-                    inputPadding={16}
-                    inputStyle={{ color: 'white' }}
-                />
-
-                <Jiro
-                    label={'Đơn giá'}
-                    labelStyle= {'italic'}
-                    borderColor={'#5aaf76'}
-                    inputPadding={16}
-                    inputStyle={{ color: 'white' }}
-                />   
-
-                <Jiro
-                    label={'Đơn vị tính'}
-                    borderColor={'#5aaf76'}
-                    inputPadding={16}
-                    inputStyle={{ color: 'white' }}
-                />
                 
-            </View>   
-     
-        </View>
+                <View style={{}}>
+                    <ScrollView>
+                        <View style={{flex: 1, marginTop: 20}}>
+                            <Animated.View style={styles.vienkhung}>
+                                <Text style={{fontSize: 20, color: 'black', fontWeight: 'bold'}}>
+                                    Thông tin dịch vụ
+                                </Text>
+
+                                <Text style={{fontSize: 13,fontWeight: 'bold', color: 'black',marginTop: 25,marginLeft: 30}}>
+                                        Loại hình dịch vụ : 
+                                </Text>
+                                <View style={{borderWidth: 2,borderColor: '#5aaf76',marginLeft: 20,marginRight:20,color: '#5aaf76',fontStyle: 'italic',marginTop: 5}}>
+                                    <RNPickerSelect
+                                        
+                                        style={{fontStyle: 'italic'}}
+                                        onValueChange={(value) => console.log(value)}
+                                        placeholder={placeholder}
+                                        useNativeAndroidPickerStyle
+                                        items={[
+                                            { label: 'Football', value: 'football',color: '#5aaf76'},
+                                            { label: 'Baseball', value: 'baseball',color: '#5aaf76'  },
+                                            { label: 'Hockey', value: 'hockey',color: '#5aaf76'  },
+                                    ]}
+                                />
+                                </View>  
+
+                                <View style={{paddingBottom: 50}}>
+                                    <Jiro
+                                        label={'Tên dịch vụ / Loại phòng'}
+                                        borderColor={'#5aaf76'}
+                                        inputPadding={16}
+                                        inputStyle={{ color: 'white' }}
+                                    />
+                                    <Jiro
+                                        label={'Đơn giá'}
+                                        borderColor={'#5aaf76'}
+                                        inputPadding={16}
+                                        inputStyle={{ color: 'white' }}
+                                    />
+                                    <Jiro
+                                        label={'Đơn vị tính'}
+                                        borderColor={'#5aaf76'}
+                                        inputPadding={16}
+                                        inputStyle={{ color: 'white' }}
+                                    />
+                                </View>
+                            </Animated.View>    
+                        </View>
+                    </ScrollView>
+                </View>    
+            </View>
         )
     }
 }
 
+
 const width = Dimensions.get("screen").width;
 var styles = StyleSheet.create({
     container: {
-      flex:0.3,
-      backgroundColor:'white',
-      justifyContent:'center' 
+        flex:1,
+        backgroundColor:'white',
+        padding: "5%"
     },
 
-    header: {
-      flex:1,
-      flexDirection: "row",
-      //backgroundColor:'white',
-      justifyContent:'center',
-      alignItems:'center'
-      
+    vienkhung:{
+        marginTop: 40,
+        paddingVertical:10,
+        borderColor: '#5aaf76',
+        borderWidth: 4,
+        padding: 15,
+        borderRadius: 20
     },
 
-    body: {
-        paddingTop: 10,
-        backgroundColor: '#E3E3E3',
-        flex:4.0,
-        //alignItems: 'center',
-        //justifyContent: 'center',
+    TextInput:{
+        borderBottomWidth: 1, 
+        paddingBottom: 0, 
+        marginTop: 5,
+        borderBottomColor:'#5aaf76',
+        fontSize: 15
+    }
 
-    },
-
-    Dropdown:{
-        fontStyle: 'italic',
-
-        paddingLeft: 20,
-        paddingStart: 20,
-        color: 'gray'
-    },
-
-    Jiro: {
-        backgroundColor: 'white',
-        marginTop:2,
-        paddingBottom:5,
-        paddingLeft: 10,
-        color:'gray',
-        fontStyle: 'italic'
-    },
 })
