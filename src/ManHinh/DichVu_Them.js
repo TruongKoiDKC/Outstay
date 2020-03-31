70bbce65dca160bb5069abbe66636fc16b393a3f
TruongKoiDKC <61343185+TruongKoiDKC@users.noreply.github.com>
2020-03-31 21:38:06 +0700

    Haizz

--- src/ManHinh/DichVu_Them.js
+++ src/ManHinh/DichVu_Them.js
@@ -114,4 +114,3 @@ var styles = StyleSheet.create({
     },
 
 })
-
--- src/ManHinh/Login.js
+++ src/ManHinh/Login.js
@@ -8,7 +8,8 @@ import {
   TextInput,
   Animated,
   Dimensions,
-  TouchableOpacity
+  TouchableOpacity,
+  console
 } from "react-native";
 import { TypingAnimation } from 'react-native-typing-animation';
 import FontAwesome  from "react-native-vector-icons/FontAwesome";
@@ -16,6 +17,7 @@ import * as Animatable from 'react-native-animatable';
 import { ScrollView } from "react-native-gesture-handler";
 import { SocialIcon } from 'react-native-elements'
 import firebase from 'react-native-firebase'
+import { GoogleSignin ,GoogleSigninButton , statusCodes} from '@react-native-community/google-signin';
 
 export default class Login extends React.Component{
   constructor(props){
@@ -103,6 +105,42 @@ export default class Login extends React.Component{
         { cancelable: false }
       )
     });
+  }
+
+  //Google Login 
+  componentDidMount(){
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId: '409483301695-ff7d5576e5vus75pr0ud2kf157hjgbc9.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      //hostedDomain: '', // specifies a hosted domain restriction
+      //loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      //accountName: '', // [Android] specifies an account name on the device that should be used
+      //iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    });
+  }
+
+  _signInGG= async()=>{
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo)
+      //this.setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+      console.log(error)
+      const {navigate} = this.props.navigation;
+      navigate('ManHinhHome');
+    }
   }
 
   render(){
@@ -200,7 +238,7 @@ export default class Login extends React.Component{
             </View>
 
             <View>
-              <TouchableOpacity onPress={() => this.props.navigation.navigate("")}>
+              <TouchableOpacity onPress={this._signInGG}>
               <SocialIcon
                 title='GMAIL'
                 button
@@ -209,6 +247,7 @@ export default class Login extends React.Component{
               </TouchableOpacity>
             </View>
           </View>    
+
 
           <TouchableOpacity onPress={() => this.props.navigation.navigate("ManHinhTaoTK")}>
             <View style={{marginTop: 30, alignItems:"center"}}>
